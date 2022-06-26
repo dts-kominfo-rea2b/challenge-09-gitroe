cc02caa264929e52c8ce2a4f511b7ae0b8626993
gitroe <106472399+gitroe@users.noreply.github.com>
2022-06-26 23:52:13 +0700

    update challenge 9

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,25 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
+    const {name, phone, email, photo} = data
     return (
-        <></>
+    <div className="container">
+      <div className="card">
+        <table>
+          <tbody>
+            <tr aria-rowspan={3}>
+              <td>
+                <img src={photo} alt={name} className="image" />
+              </td>
+              <tr className="strong">{name}</tr>
+              <tr className="strong">{phone}</tr>
+              <tr className="strong">{email}</tr>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     )
 }
 
